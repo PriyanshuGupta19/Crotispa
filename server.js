ddf7d17b7dbebf6449cfc54ac843b50a5d27157c
Priyanshu Gupta <priyanshugupta2028.com>
2024-05-15 22:55:03 +0530

    s

---
+++ server.js
@@ -0,0 +1,8 @@
+const path = require('path');
+const app = require('./backend/app');
+
+// Start the server
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
